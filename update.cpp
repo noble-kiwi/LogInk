4e8cdf302295a5f32671dc36ba396973616c333d
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:49:09 +0300

    Update update.cpp

--- update.cpp
+++ update.cpp
@@ -1,6 +1,8 @@
 #include "Engine.h"
 
 void Engine::update(float dtAsSeconds) {
-    // Update car position based on current input and elapsed time
-    m_Car.update(dtAsSeconds);
+    // Update the logical vehicle first
+    m_Vehicle.update(dtAsSeconds);
+    // Then synchronise the car's graphics
+    m_Car.update();
 }
