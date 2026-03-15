b505190205cdd596809ee8d91a1382d9871ffd14
noble_kiwi <iwkr1208@gmail.com>
2026-03-15 16:51:26 +0300

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,29 +1,17 @@
 #include "Car.h"
 #include "TextureHolder.h"
 
-Car::Car(InputHandler& input) : m_Input(input) {
-    m_Speed = 400.0f;
-    // Load the car texture using the TextureHolder singleton.
+Car::Car(const Vehicle& vehicle) : m_Vehicle(vehicle) {
     m_Sprite.setTexture(TextureHolder::getTexture("Image/Car.png"));
-    m_Position = {10.0f, 25.0f};
-    m_Sprite.setPosition(m_Position);
+    // Initial position will be set in update, but we can set it here too
+    m_Sprite.setPosition(m_Vehicle.getPosition());
 }
 
-void Car::update(float elapsedTime) {
-    // Move according to pressed keys.
-    // Note: horizontal speeds are intentionally different (left slower, right faster)
-    if (m_Input.isUpPressed()) {
-        m_Position.y -= m_Speed * elapsedTime;
-    }
-    if (m_Input.isDownPressed()) {
-        m_Position.y += m_Speed * elapsedTime;
-    }
-    if (m_Input.isLeftPressed()) {
-        m_Position.x -= (m_Speed * elapsedTime) / 2.0f;
-    }
-    if (m_Input.isRightPressed()) {
-        m_Position.x += (m_Speed * elapsedTime) * 1.25f;
-    }
-    // Apply the new position to the sprite
-    m_Sprite.setPosition(m_Position);
+void Car::update() {
+    // Sync sprite position with vehicle model
+    m_Sprite.setPosition(m_Vehicle.getPosition());
+}
+
+void Car::draw(sf::RenderWindow& window) const {
+    window.draw(m_Sprite);
 }
